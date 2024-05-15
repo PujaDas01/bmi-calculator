9757772b1b8805e9b1d3f5ccf41cea3ef3bc9c59
Puja Das <102240706+PujaDas@users.noreply.github.com>
2024-05-15 17:06:02 +0530

    Worked on UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [weight, setWeight] = useState('');
+  const [height, setHeight] = useState('');
+
+
+  const calculateBMI = () => {
+    console.log('Button Fired!');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="header">
+          <h1>BMI Calculator</h1>
+        </div>
+        <div className="input-group">
+          <label htmlFor="weight">Weight (kg):</label>
+          <input
+            type="number"
+            id="weight"
+            value={weight}
+            onChange={(e) => setWeight(e.target.value)}
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="height">Height (cm):</label>
+          <input
+            type="number"
+            id="height"
+            value={height}
+            onChange={(e) => setHeight(e.target.value)}
+          />
+        </div>
+        <div className="button-container">
+          <button className="button" onClick={calculateBMI}>
+            Calculate BMI
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
